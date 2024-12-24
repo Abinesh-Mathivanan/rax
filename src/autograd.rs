d1213ad92e80979dfc81a9970b8658cc25f04c8e
Abinesh-Mathivanan <abineshmathivanan31@gmail.com>
2024-12-24 22:12:57 +0530

    added dyanmic graph autograd - only for add - unstable

--- src/autograd.rs
+++ src/autograd.rs
@@ -1,102 +1,92 @@
-use std::rc::Rc;
+use ndarray::Array;
 use std::cell::RefCell;
+use std::rc::{Rc, Weak};
 
 #[derive(Clone)]
 pub struct Tensor {
-    data: f64,
-    grad: f64,
-    grad_fn: Option<Rc<RefCell<dyn FnMut(f64)>>>,
+    pub data: Array<f64, ndarray::IxDyn>,
+    pub grad: Option<Array<f64, ndarray::IxDyn>>,
+    pub requires_grad: bool,
+    pub creator: Option<Weak<RefCell<GraphNode>>>,
 }
 
 impl Tensor {
-    pub fn new(data: f64) -> Self {
+    pub fn new(data: Array<f64, ndarray::IxDyn>, requires_grad: bool) -> Self {
         Tensor {
             data,
-            grad: 0.0,
-            grad_fn: None,
-        }
-    }
-
-    pub fn data(&self) -> f64 {
-        self.data
-    }
-
-    pub fn grad(&self) -> f64 {
-        self.grad
-    }
-
-    pub fn set_grad(&mut self, grad: f64) {
-        self.grad = grad;
-    }
-
-    pub fn set_grad_fn<F>(&mut self, grad_fn: F)
-    where
-        F: 'static + FnMut(f64),
-    {
-        self.grad_fn = Some(Rc::new(RefCell::new(grad_fn)));
-    }
-
-    pub fn backward(&mut self, grad: f64) {
-        self.grad += grad;
-        if let Some(ref grad_fn) = self.grad_fn {
-            grad_fn.borrow_mut()(grad);
+            grad: None,
+            requires_grad,
+            creator: None,
         }
     }
 
     pub fn zero_grad(&mut self) {
-        self.grad = 0.0;
-    }
-}
-
-impl std::ops::Add for &Tensor {
-    type Output = Tensor;
-
-    fn add(self, other: Self) -> Tensor {
-        let mut result = Tensor::new(self.data + other.data);
-        let self_clone = Rc::new(RefCell::new(self.clone()));
-        let other_clone = Rc::new(RefCell::new(other.clone()));
-        result.set_grad_fn(move |grad| {
-            self_clone.borrow_mut().backward(grad);
-            other_clone.borrow_mut().backward(grad);
-        });
-        result
-    }
-}
-
-impl std::ops::Mul for &Tensor {
-    type Output = Tensor;
-
-    fn mul(self, other: Self) -> Tensor {
-        let mut result = Tensor::new(self.data * other.data);
-        let self_clone = Rc::new(RefCell::new(self.clone()));
-        let other_clone = Rc::new(RefCell::new(other.clone()));
-        let other_data = other.data;
-        let self_data = self.data;
-        result.set_grad_fn(move |grad| {
-            self_clone.borrow_mut().backward(grad * other_data);
-            other_clone.borrow_mut().backward(grad * self_data);
-        });
-        result
-    }
-}
-
-// Optimizer
-pub struct SGD {
-    lr: f64,
-}
-
-impl SGD {
-    pub fn new(lr: f64) -> Self {
-        SGD { lr }
+        self.grad = None;
     }
 
-    pub fn step(&self, params: &mut [Tensor]) {
-        for param in params {
-            let grad = param.grad();
-            let data = param.data();
-            param.set_grad(0.0);
-            param.set_grad(data - self.lr * grad);
+    pub fn backward(&mut self) {
+        if self.grad.is_none() {
+            self.grad = Some(Array::ones(self.data.raw_dim()));
+        }
+
+        let mut stack = vec![Rc::new(RefCell::new(self.clone()))];
+
+        while let Some(node) = stack.pop() {
+            if let Some(creator_weak) = &node.borrow().creator {
+                if let Some(creator) = creator_weak.upgrade() {
+                    let grad = node.borrow().grad.clone().unwrap();
+
+                    {
+                        let backward_fn = &creator.borrow().backward_fn;
+                        backward_fn(&grad, &mut creator.borrow_mut().inputs);
+                    }
+
+                    for input in &creator.borrow().inputs {
+                        stack.push(input.clone());
+                    }
+                }
+            }
         }
     }
 }
 
+pub struct GraphNode {
+    pub operation: String,
+    pub inputs: Vec<Rc<RefCell<Tensor>>>,
+    pub backward_fn: Box<dyn Fn(&Array<f64, ndarray::IxDyn>, &mut Vec<Rc<RefCell<Tensor>>>)>,
+}
+
+impl GraphNode {
+    pub fn new(
+        operation: String,
+        inputs: Vec<Rc<RefCell<Tensor>>>,
+        backward_fn: Box<dyn Fn(&Array<f64, ndarray::IxDyn>, &mut Vec<Rc<RefCell<Tensor>>>)>,
+    ) -> Self {
+        GraphNode {
+            operation,
+            inputs,
+            backward_fn,
+        }
+    }
+}
+
+pub fn add(tensor1: &Rc<RefCell<Tensor>>, tensor2: &Rc<RefCell<Tensor>>) -> Rc<RefCell<Tensor>> {
+    let data = &tensor1.borrow().data + &tensor2.borrow().data;
+    let requires_grad = tensor1.borrow().requires_grad || tensor2.borrow().requires_grad;
+
+    let output = Rc::new(RefCell::new(Tensor::new(data, requires_grad)));
+
+    if requires_grad {
+        let node = GraphNode::new(
+            "add".to_string(),
+            vec![tensor1.clone(), tensor2.clone()],
+            Box::new(move |grad, inputs| {
+                inputs[0].borrow_mut().grad = Some(grad.clone());
+                inputs[1].borrow_mut().grad = Some(grad.clone());
+            }),
+        );
+        output.borrow_mut().creator = Some(Rc::downgrade(&Rc::new(RefCell::new(node))));
+    }
+
+    output
+}
